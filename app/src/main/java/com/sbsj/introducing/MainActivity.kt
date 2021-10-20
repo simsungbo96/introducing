fc56cc7384a7ebdd9c94f496b1cdac7202a9a2b8
master <simsungbo96@gmail.com>
2021-10-20 19:45:17 +0900

    title 수정

--- app/src/main/java/com/sbsj/introducing/MainActivity.kt
+++ app/src/main/java/com/sbsj/introducing/MainActivity.kt
@@ -12,19 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var dTv = findViewById<TextView>(R.id.tv_D)
+        var rTv = findViewById<TextView>(R.id.tv_r)
+        var realTv = findViewById<TextView>(R.id.tv_real)
         var btn = findViewById<Button>(R.id.button)
         var btn2 = findViewById<Button>(R.id.button2)
 
 
         btn.setOnClickListener {
             var vis  = AlphaAnimation(0f,1f)
+            var gon = AlphaAnimation(1f,0f)
             vis.duration = 1000;
+            gon.duration =1000;
+            rTv.visibility =View.VISIBLE
             dTv.visibility = View.VISIBLE
+            realTv.visibility = View.GONE
             dTv.animation = vis;
+            rTv.animation =vis;
+            realTv.animation = gon;
 
         }
         btn2.setOnClickListener{
             dTv.visibility = View.GONE
+            rTv.visibility = View.GONE
+            realTv.visibility = View.VISIBLE
         }
     }
 }
